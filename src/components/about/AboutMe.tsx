53636d986fc017d83ca4fb68d45e8e5860f5b09a
Firstname Lastname <jaiswalmihir.business@gmail.com>
2024-08-08 14:02:14 +0530

    some changes

--- src/components/about/AboutMe.tsx
+++ src/components/about/AboutMe.tsx
@@ -7,6 +7,8 @@ import { Favourite } from './Favorite';
 import { aboutImages } from '../../../constant';
 import { MotionDiv } from '../MotiionDiv';
 
+const ANIMATION_DURATION = 0.5;
+
 const AboutMe: React.FC = () => {
   return (
     <MotionDiv
@@ -15,32 +17,57 @@ const AboutMe: React.FC = () => {
       exit={{ opacity: 0 }}
       className="flex flex-col items-center space-y-12 px-6 pb-6 overflow-x-hidden"
     >
-      <div className="max-w-4xl space-y-6">
+      <MotionDiv
+        initial={{ translateY: 20, opacity: 0 }}
+        animate={{ translateY: 0, opacity: 1, transition: { duration: ANIMATION_DURATION } }}
+        className="max-w-4xl space-y-6"
+      >
         <Header underlineColor="#ff9400" emoji='❤️' className="text-3xl font-bold text-gray-900 dark:text-gray-100">About Me</Header>
         <p className="md:text-xl text-gray-700 dark:text-gray-300 leading-relaxed">
           <span className="block md:inline mb-2">Since I was young, I've loved computers.</span> 
           <span className="block md:inline mb-2">I've also been pretty creative, spending my free time drawing and painting.</span> 
           <span className="block md:inline">This creativity helps me express myself more in web applications.</span>
         </p> 
-      </div>
-      <div className=''>
-        <div className="hidden md:block">
-          <ScatteredLine images={aboutImages} />
+      </MotionDiv>
+
+      <MotionDiv
+        initial={{ translateY: 20, opacity: 0 }}
+        animate={{ translateY: 0, opacity: 1, transition: { duration: ANIMATION_DURATION, delay: 0.1 } }}
+      >
+        <div className=''>
+          <div className="hidden md:block">
+            <ScatteredLine images={aboutImages} />
+          </div>
+          <div className="md:hidden">
+            <ScatteredLine2 images={aboutImages} />
+          </div>
         </div>
-        <div className="md:hidden">
-          <ScatteredLine2 images={aboutImages} />
-        </div>
-      </div>
-      <span className="block md:inline md:text-xl text-gray-700 dark:text-gray-300 leading-relaxed max-w-4xl mx-auto ">
+      </MotionDiv>
+
+      <MotionDiv
+        initial={{ translateY: 20, opacity: 0 }}
+        animate={{ translateY: 0, opacity: 1, transition: { duration: ANIMATION_DURATION, delay: 0.2 } }}
+        className="block md:inline md:text-xl text-gray-700 dark:text-gray-300 leading-relaxed max-w-4xl mx-auto"
+      >
         Beyond software development, I enjoy anime, drawing, cooking, reading books and writing stories. ✨
-      </span>
-      <div className='max-w-4xl mx-auto'>
-      <Header underlineColor="#ff9400" className="text-3xl font-bold text-gray-900 dark:text-gray-100 pb-6" >Days I code</Header>
-       <div className='md:w-full w-80'>
-       <GitHubContributionGraph/>
-       </div>
-      </div>
-      <div className="mt-8">
+      </MotionDiv>
+
+      <MotionDiv
+        initial={{ translateY: 20, opacity: 0 }}
+        animate={{ translateY: 0, opacity: 1, transition: { duration: ANIMATION_DURATION, delay: 0.3 } }}
+        className='max-w-4xl mx-auto'
+      >
+        <Header underlineColor="#ff9400" className="text-3xl font-bold text-gray-900 dark:text-gray-100 pb-6">Days I code</Header>
+        <div className='md:w-full w-80'>
+          <GitHubContributionGraph />
+        </div>
+      </MotionDiv>
+
+      <MotionDiv
+        initial={{ translateY: 20, opacity: 0 }}
+        animate={{ translateY: 0, opacity: 1, transition: { duration: ANIMATION_DURATION, delay: 0.4 } }}
+        className="mt-8"
+      >
         <a
           href="/assests/resume.pdf" 
           download
@@ -49,12 +76,16 @@ const AboutMe: React.FC = () => {
           <FaDownload size={16} />
           <span>Download My Resume</span>
         </a>
-      </div>
-      
-        <div className='w-full max-w-4xl'>
+      </MotionDiv>
+
+      <MotionDiv
+        initial={{ translateY: 20, opacity: 0 }}
+        animate={{ translateY: 0, opacity: 1, transition: { duration: ANIMATION_DURATION, delay: 0.5 } }}
+        className='w-full max-w-4xl'
+      >
         <Header underlineColor="#ff9400" className="text-3xl font-bold text-gray-900 dark:text-gray-100 pb-3">Hobbies</Header>
-        <Favourite/>
-        </div>
+        <Favourite />
+      </MotionDiv>
     </MotionDiv>
   );
 };
